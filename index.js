9393c0bfbf0f44db57fd9793eebc9b803ce5da75
Joyeeta Sarkar <joyeetasarkar1410@gmail.com>
2023-05-21 02:27:36 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,10 +2,39 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // middle-wares
 app.use(cors());
 app.use(express.json());
+
+console.log();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASSWORD}@cluster0.aibkcfj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+     client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //  client.close();
+  }
+}
+run().catch(console.log);
+
 
 app.get('/', (req, res)=>{
     console.log('Connected');
