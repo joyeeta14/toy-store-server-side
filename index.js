a6691dc0fafc018b33b93e88a8820fe9bf842435
Joyeeta Sarkar <joyeetasarkar1410@gmail.com>
2023-05-21 01:45:08 +0600

    middle-wares added

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-
+// middle-wares
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res)=>{
     console.log('Connected');
